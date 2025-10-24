b4e58c6ed9dbb4f1bbd64b330e3674f39390999b
heiphin7 <152867030+heiphin7@users.noreply.github.com>
2025-10-24 16:58:35 +0500

    Update SecurityConfig.java

--- src/main/java/com/qdeb/config/SecurityConfig.java
+++ src/main/java/com/qdeb/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:8000", "http://127.0.0.1:8000"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
