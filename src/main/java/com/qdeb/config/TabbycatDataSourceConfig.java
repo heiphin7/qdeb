a0152d013f118ca47a756c7306c9602a65c06754
ondyy <ondyyvfx@gmail.com>
2026-06-02 03:00:10 +0500

    fix datasource conflict: explicit primary datasource + DriverManagerDataSource for tabbycat

--- src/main/java/com/qdeb/config/TabbycatDataSourceConfig.java
+++ src/main/java/com/qdeb/config/TabbycatDataSourceConfig.java
@@ -1,21 +1,40 @@
 package com.qdeb.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class TabbycatDataSourceConfig {
 
+    @Primary
+    @Bean(name = "dataSourceProperties")
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties primaryDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = "dataSource")
+    public DataSource primaryDataSource() {
+        return primaryDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
     @Bean(name = "tabbycatDataSource")
-    @ConfigurationProperties(prefix = "tabbycat.datasource")
     public DataSource tabbycatDataSource() {
-        return DataSourceBuilder.create().build();
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl("jdbc:postgresql://172.18.0.3:5432/tabbycat");
+        ds.setUsername("tabbycat");
+        ds.setPassword("tabbycat");
+        return ds;
     }
 
     @Bean(name = "tabbycatJdbcTemplate")
